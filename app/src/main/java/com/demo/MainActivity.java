ff6d33a579aa2de81968ca57c735dccabb6ae653
SolomonVP <Lum7023@yandex.ru>
2023-06-03 22:11:35 +0300

    onPause, onResume

--- app/src/main/java/com/demo/MainActivity.java
+++ app/src/main/java/com/demo/MainActivity.java
@@ -25,16 +25,29 @@ public class MainActivity extends AppCompatActivity {
         runTimer();
     }
 
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        wasRunning = isRunning;
+//        isRunning = false;
+//    }
+
     @Override
-    protected void onStop() {
-        super.onStop();
+    protected void onPause() {
+        super.onPause();
         wasRunning = isRunning;
         isRunning = false;
     }
 
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        isRunning = wasRunning;
+//    }
+
     @Override
-    protected void onStart() {
-        super.onStart();
+    protected void onResume() {
+        super.onResume();
         isRunning = wasRunning;
     }
 
